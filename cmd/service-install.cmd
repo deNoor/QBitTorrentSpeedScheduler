c227253e3ff9018f21f877997f628855373a9e05
deNoor <denoorx@gmail.com>
2025-01-06 18:05:31 +0300

    Update to NET 9.0.

--- cmd/service-install.cmd
+++ cmd/service-install.cmd
@@ -1,5 +1,5 @@
 cd /d %~dp0
 cd ..
-sc create "qBitTorrent speed scheduler" binpath="%cd%\QBitTorrentSpeedScheduler.exe" start=auto
+sc create "qBitTorrent speed scheduler" binpath="%cd%\QBitTorrentSpeedScheduler.exe" start=auto obj="NT AUTHORITY\Local Service" password=""
 net start "qBitTorrent speed scheduler"
 pause
